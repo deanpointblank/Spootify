39173851812f2f169460f5d5a31afc6b866754f4
dean <deanlloyd64@gmail.com>
2019-12-03 22:05:29 -0500

    create library, music, song, album, podcast, show, episode resources. Set devise to use with custom registrations controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :episodes
+  resources :shows
+  resources :podcasts
+  resources :songs
+  resources :albums
+  resources :musics
+  resources :libraries
+  devise_for :users, :controllers => {registrations: 'registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'application#welcome'
 end
