bdce0186486fc4f9beaf6f4698e71e0d279ca3c5
dean <deanlloyd64@gmail.com>
2019-12-04 00:25:08 -0500

    narrow down routes to essentials

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   
-  resources :episodes
-  resources :shows
-  resources :podcasts
-  resources :songs
-  resources :albums
-  resources :musics
-  resources :libraries
+  resources :episodes, only: [:new, :create, :show]
+  resources :shows, only: [:new, :create, :index, :show]
+  resources :podcasts, only: [:index]
+  resources :songs, only: [:new, :create]
+  resources :albums, only: [:new, :create, :show]
+  resources :musics, only: [:index]
+  resources :libraries, only: [:index]
 
   devise_for :users, :controllers => {registrations: 'registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
