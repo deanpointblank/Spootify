b2c19182f0eb3f3ccee0ac194536770f755c556f
dean <deanlloyd64@gmail.com>
2019-12-04 13:26:14 -0500

    add omniauth.rb with facebook and google strategies.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    provider :google, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+end
