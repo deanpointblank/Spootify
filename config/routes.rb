54757b1ea90ce7c4259b6c85eacb3f10dc233817
dean <deanlloyd64@gmail.com>
2019-12-04 11:48:23 -0500

    add further resources to routes, added spec file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   
-  resources :episodes, only: [:new, :create, :show]
-  resources :shows, only: [:new, :create, :index, :show]
+  resources :episodes, only: [:new, :create, :show, :edit, :update, :destroy]
+  
+  resources :shows, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  
   resources :podcasts, only: [:index]
-  resources :songs, only: [:new, :create]
-  resources :albums, only: [:new, :create, :show]
+  
+  resources :songs, only: [:new, :create, :edit, :update, :destroy]
+  
+  resources :albums, only: [:new, :create, :show, :edit, :update, :destroy]
+  
   resources :musics, only: [:index]
+  
   resources :libraries, only: [:index]
 
   devise_for :users, :controllers => {registrations: 'registrations'}
