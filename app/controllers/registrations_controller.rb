ed94d0ad8ad72d9dbba222de2d9e54d4da44345a
dean <deanlloyd64@gmail.com>
2019-12-06 14:50:22 -0500

    correct registration strong params comments

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
 
     private
 
-    ############################# Come Back to this, this is copied from the lecture ###################
+    ############################# All working ###################
     def sign_up_params
         params.require(:user).permit(:username, :email, :password, :password_confirmation, :admin, :gender, :birthday)
     end
