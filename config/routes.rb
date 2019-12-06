c5c4ccd73860413c6d5a76aec36a47d751306ab5
dean <deanlloyd64@gmail.com>
2019-12-06 14:10:58 -0500

    generate users controller, create user show view, modify nav to link to current_user_path

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   
 
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: "users/omniauth_callbacks"}
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   ########## AdminSpecific-Rotues ##########
