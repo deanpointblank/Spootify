89345b564701e05fa39fd3e84d54ccb6b58780c9
dean <deanlloyd64@gmail.com>
2019-12-07 16:23:30 -0500

    add restful routes to artist/albums

--- app/controllers/artists/albums_controller.rb
+++ app/controllers/artists/albums_controller.rb
@@ -1,2 +1,39 @@
 class Artists::AlbumsController < ApplicationController
+
+    def index
+        @albums = current_user.albums_made
+    end
+
+    def new
+        @album = Album.new
+    end
+
+    def create
+        @album = Album.new(album_params)
+    end
+
+    def show
+        @album = Album.find_by(id: params[:id])
+    end
+
+    def edit
+        @album = Album.find_by(id: params[:id])
+    end
+
+    def update
+        @album = Album.find_by(id: params[:id])
+
+        @album.update(album_params)
+
+        if @album.save
+            redirect_to @album
+        else
+            render 'edit'
+        end
+
+    end
+
+    private 
+
+
 end
