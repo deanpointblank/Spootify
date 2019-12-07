da749dad0e37d51757cdc2dda3d1e43d3aaf3c2a
dean <deanlloyd64@gmail.com>
2019-12-06 20:43:41 -0500

    add routes for music, podcast and appropriate nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   ########### Podcast-Routes ##############
 
-  resources :podcasts, only: [:index]
+  resources :podcasts, only: [:index] do
+    resources :shows
+  end
   
   resources :shows do
     resources :episodes, except: [:index, :update, :destroy]
@@ -12,7 +14,9 @@ Rails.application.routes.draw do
   
   ############ Music-Routes ##############
 
-  resources :musics, only: [:index]
+  resources :musics, only: [:index] do
+    resources :albums
+  end
 
   resources :albums, except: [:index] do
     resources :songs, only: [:new, :create, :edit]
@@ -24,16 +28,22 @@ Rails.application.routes.draw do
   ############ User-Routes ################
   
 
-  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: "users/omniauth_callbacks"}
-  resources :users, only: [:show, :index] do
-    ########### Library-Routes ##############
-    resources :libraries, only: [:index]
-    get :home, to: 'libraries#home'
-    get :playlists, to: 'libraries#playlists'
-    get :made_for_you, to: 'libraries#made_for_you'
-    get :liked_songs, to: 'libraries#liked_songs'
-  end
+    devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: "users/omniauth_callbacks"}
+    resources :users, only: [:show, :index] do
+      ########### User-Library-Routes ##############
+      resources :libraries, only: [:index]
+      get :home, to: 'libraries#home'
+      get :playlists, to: 'libraries#playlists'
+      get :made_for_you, to: 'libraries#made_for_you'
+      get :liked_songs, to: 'libraries#liked_songs'
+    end
+  
+  ########### Library-Routes ##############
+  resources :libraries, only: [:index] do
+    resources :musics, only: [:index]
+    resources :podcasts, only: [:index]
 
+  end
     
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
