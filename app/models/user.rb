11a57ad4d55b027348146084791ced677aa18221
dean <deanlloyd64@gmail.com>
2019-12-07 01:05:32 -0500

    add conditional has_many statements to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook]
 
   has_one :library
-  has_many :albums_made, foreign_key: "artist_id", class_name: "Album"
+  has_many :albums_made, -> { where artist: true }, foreign_key: "artist_id", class_name: "Album"
+  has_many :
   #has_many :songs, through: :albums_made, source:
   
   def self.new_with_session(params, session)
