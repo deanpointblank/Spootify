d4b8148eb2b8a299581830f3b7223ced51e457d0
dean <deanlloyd64@gmail.com>
2019-12-06 20:07:12 -0500

    add custom library routes

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,2 +1,16 @@
 class LibrariesController < ApplicationController
+
+    def home
+    end
+
+    def playlists
+    end
+
+    def made_for_you
+    end
+
+    def liked_songs
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  ########### Library-Routes ##############
-
-  resources :libraries, only: [:index]
-
   ########### Podcast-Routes ##############
 
   resources :podcasts, only: [:index]
@@ -29,10 +25,22 @@ Rails.application.routes.draw do
   
 
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: "users/omniauth_callbacks"}
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index] do
+    ########### Library-Routes ##############
+    resources :libraries, only: [:index]
+    get :home, to: 'libraries#home'
+    get :playlists, to: 'libraries#playlists'
+    get :made_for_you, to: 'libraries#made_for_you'
+    get :liked_songs, to: 'libraries#liked_songs'
+  end
+
+    
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   ########## AdminSpecific-Rotues ##########
+
+
 
 
 
